10ea8e55ca6f97247f130b55f2f948b09344eaaf
Jefry <jnavass@est.ups.edu.ec>
2021-05-07 11:58:02 -0500

    Colocar colores

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 const { argv } = require("./config/yargs");
 const { crear, listar, actualizar, eliminar } = require("./controlador/tareas")
-
+const colors = require("colors")
 let comando = argv._[0];
 
 switch (comando) {
     case "crear":
-        console.log("Creando una tarea...");
+        console.log("Creando una tarea...".yellow);
         let tarea = crear(argv.descripcion);
-        console.log(tarea);
-        console.log("Tarea Guardada");
+        console.log("tarea: ", tarea.descripcion);
+        console.log("Tarea Guardada Correctamente".green);
         break;
     case "listar":
-        console.log("Listando tareas...");
+        console.log("Listando tareas...".yellow);
         listar();
         break;
 
     case "actualizar":
-        console.log("Actualizando la tarea...");
+        console.log("Actualizando la tarea...".yellow);
         let res = actualizar(argv.descripcion, argv.completado)
-        if (res) console.log("Tarea actualizada correctamente");
-        else console.log("No se ha encontrado la tarea");
+        if (res) console.log("Tarea actualizada correctamente".green);
+        else console.log("No se ha encontrado la tarea".yellow);
         break;
     case "eliminar":
-        console.log("Eliminando tarea...");
+        console.log("Eliminando tarea...".yellow);
         let result = eliminar(argv.descripcion);
-        if (result) console.log("Tarea eliminada correctamente");
-        else console.log("No se ha encontrado la tarea");
+        if (result) console.log("Tarea eliminada correctamente".green);
+        else console.log("No se ha encontrado la tarea".yellow);
         break;
 
     default:
